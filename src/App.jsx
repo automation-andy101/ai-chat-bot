fd6a77baaed238a52cfce33048aed905e035853a
automation.andy101 <automation.andy101@gmail.com>
2025-07-19 22:00:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ChatBotStart from './components/ChatBotStart'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <ChatBotStart />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ChatBotStart.jsx
@@ -0,0 +1,11 @@
+import './ChatBotStart.css';
+
+const ChatBotStart = () => {
+  return (
+    <div className='start-page'>
+      <button className="start-page-button">Chat AI</button>
+    </div>
+  )
+}
+
+export default ChatBotStart
